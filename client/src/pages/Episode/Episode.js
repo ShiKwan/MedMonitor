b30c62760afe26994fc68e9a8fc03f9478c51c61
ShiKwan <31040895+ShiKwan@users.noreply.github.com>
2018-03-22 23:18:02 -0400

    created main pages, kept the book pages for reference

---
+++ client/src/pages/Episode/Episode.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './Episode.css';
+
+class Episode extends Component {
+    state = {
+
+    };
+    render() {
+        return (
+            <div>
+                Episode Page
+            </div>
+        )
+    }
+}
+
+export default Episode;
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './Home.css';
+
+class Home extends Component {
+    state = {
+
+    };
+    render() {
+        return (
+            <div>
+                Home Page
+            </div>
+        )
+    }
+}
+
+export default Home;
