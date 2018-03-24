20276029952000b9c9ae0b9064759006f17e288f
ShiKwan <31040895+ShiKwan@users.noreply.github.com>
2018-03-24 12:24:12 -0400

    created new test pages for react-straping and testing routes and database

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import SK from "./pages/SK";
+import MH from "./pages/MH";
+import BM from "./pages/BM";
+import JM from "./pages/JM";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -13,6 +16,9 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Books} />
         <Route exact path="/sk" component={SK} />
+        <Route exact path="/mh" component={MH} />
+        <Route exact path="/bm" component={BM} />
+        <Route exact path="/jm" component={JM} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
---
+++ client/src/pages/BS/index.js
@@ -0,0 +1,1 @@
+export { default } from "./BH.js";
