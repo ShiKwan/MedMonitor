c1d0e87826e6608cfaf956ba3efea7c79ba35a91
Shi-Kwan <sktan@insytecg.com>
2018-03-24 01:19:03 -0400

    added medication API into API folder.

--- client/src/utils/medicationAPI.js
+++ client/src/utils/medicationAPI.js
@@ -1,6 +1,24 @@
 import axios from "axios";
 export default {
+
+    findAll: function() {
+        return axios.get("/api/medication");
+    }, 
+
     newDrug: function(drugInfo) {
         return axios.post("/api/medication", drugInfo);
+    },
+
+    editDose: function(id, doseInfo){
+        return axios.put("/api/medication/dose/"+id, doseInfo);
+    },
+
+    deleteDose: function(id){
+        return axios.delete("/api/medication/dose/"+id);
+    },
+
+    deleteMedication: function(id){
+        return axios.delete("/api/medication/"+id)
     }
+
 };
