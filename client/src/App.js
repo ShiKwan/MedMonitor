22c3d08283969c3d9a42499f0cbcf723a41f9cb8
Shi-Kwan <sktan@insytecg.com>
2018-03-24 00:21:52 -0400

    did a yarn build, and added personal workspace in pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
+import SK from "./pages/SK";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -11,6 +12,7 @@ const App = () => (
       <Nav />
       <Switch>
         <Route exact path="/" component={Books} />
+        <Route exact path="/sk" component={SK} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
