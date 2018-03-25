c72a1f5f383f3344bffafae5dbee9901ea66eb03
Shi-Kwan <sktan@insytecg.com>
2018-03-25 16:50:10 -0400

    added react barebone script into those two freshly created pages.

---
+++ client/src/pages/Admin_Episode/Admin_Episode.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Admin_Episode extends Component {
+    state = {
+
+    };
+    render() {
+        return (
+            <div>
+                Admin Episode Page
+            </div>
+        )
+    }
+}
+
+export default Admin_Episode;
---
+++ client/src/pages/Admin_Report/Admin_Report.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Admin_Report extends Component {
+    state = {
+
+    };
+    render() {
+        return (
+            <div>
+                Admin Report Page
+            </div>
+        )
+    }
+}
+
+export default Admin_Report;
