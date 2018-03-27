24be09dfb1bdfb283480a7a7ebfdab581b1dbb2b
unknown <bradwayne74@gmail.com>
2018-03-27 11:12:40 -0400

    appointment card modify

--- client/src/components/PhysInfo/PhysInfo.js
+++ client/src/components/PhysInfo/PhysInfo.js
@@ -18,28 +18,25 @@ import {
 const PhysInfo = (props) => {
 
     return (
-        <Container className="AppointmentCards">
+        <Container className="AppointmentCard">
             <Card className="physInfoCard" body outline color="warning">
                 <CardTitle className="physInfoHeader">Physician Information</CardTitle>
-                <Label for="patientDoc">Doctor</Label>
-                <Input type="text" size="lg" name="email" id="PatDocName" placeholder="doctor name" />
-
-                <Label for="patientDocAddress">Address</Label>
-                <Input type="text" size="lg" name="email" id="PatDocAddress" placeholder="doctor address" />
-
-                <Label for="patientDocCity">City</Label>
-                <Input type="text" size="lg" name="email" id="PatDocCity" placeholder="doctor city" />
-
-                <Label for="patientDocPhNum">Phone #</Label>
-                <Input type="text" size="lg" name="email" id="PatientDocNum" placeholder="doctor phone number" />
-
+                <Label className="patDoc" for="patDoc">Doctor: </Label>
+                <br>
+                </br>
+                <Label className="patDocAddress" for="patDocAddress">Address: </Label>
+                <br>
+                </br>
+                <Label className="patDocCity" for="patDocCity">City: </Label>
+                <br>
+                </br>
+                <Label className="patDocPhNum" for="patDocPhNum">Phone Number: </Label>
                 <CardTitle className="physInfoHours">Office Hours</CardTitle>
-
-                <Label for="patientDocPhNum">From</Label>
-                <Input type="text" size="lg" name="email" id="PatDocOffHrFrom" placeholder="office hour from" />
-
-                <Label for="patientDocPhNum">To</Label>
-                <Input type="text" size="lg" name="email" id="PatDocOffHrTo" placeholder="office hours to" />
+             
+                <Label className="patDocPhNum" for="patDocPhNum">From: </Label>
+                <br>
+                </br>
+                <Label className="patDocPhNum" for="patDocPhNum">To: </Label>
             </Card>
         </Container>
     );
