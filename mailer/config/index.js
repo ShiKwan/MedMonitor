8b98007473551c3a0cb910506ab275a1c773adcc
unknown <bradwayne74@gmail.com>
2018-03-28 16:35:56 -0400

    patient page survey

--- mailer/config/index.js
+++ mailer/config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
     GOOGLE_API_KEY: 'AIzaSyAfOfPHWIzdLUT_2pslxBjVVaBYCqnVKBE',
-    CLIENT_ID : '952732101875-gfis7sk3as9t1f59j3h07a2aaugto4ku.apps.googleusercontent.com',
-    CLIENT_SECRET : 'pRgJQKnKbuE0OGQPZzqp4YKW',
-    REFRESHTOKEN : "1/08dSgI2Rckdt6Z5pHlHBg4S8svb8eykJXH0ocB78cjc",
-    ACCESSTOKEN : "ya29.GluKBR-Yzx_J-hec12-PybtdFljRGGQd-NvcHhDrOzxVwgdE0pim9tJfWyE9Wa_FJGkeuWu6hG9WkkZ3RMh1odexDrcPnvgmaviMKS-2RhpsKWDPghXlnpXzFzSd",
-    USER: 'bscwruproject2@gmail.com', 
+    CLIENT_ID: '952732101875-gfis7sk3as9t1f59j3h07a2aaugto4ku.apps.googleusercontent.com',
+    CLIENT_SECRET: 'pRgJQKnKbuE0OGQPZzqp4YKW',
+    REFRESHTOKEN: "1/08dSgI2Rckdt6Z5pHlHBg4S8svb8eykJXH0ocB78cjc",
+    ACCESSTOKEN: "ya29.GluKBR-Yzx_J-hec12-PybtdFljRGGQd-NvcHhDrOzxVwgdE0pim9tJfWyE9Wa_FJGkeuWu6hG9WkkZ3RMh1odexDrcPnvgmaviMKS-2RhpsKWDPghXlnpXzFzSd",
+    USER: 'bscwruproject2@gmail.com',
     PASS: 'Bootcamp123'
 }
