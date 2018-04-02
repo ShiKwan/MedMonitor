3d83c88dc0a30e63a47e4d9cd9027302b6332851
Shi-Kwan <sktan@insytecg.com>
2018-04-01 23:53:11 -0400

    file renamed

--- client/src/components/Admin/episode/PreviousMedication/PreviousMedication.js
+++ client/src/components/Admin/episode/PreviousMedication/PreviousMedication.js
@@ -11,7 +11,7 @@ import {
 } from 'reactstrap';
 
 
-export default class PatientMedication extends React.Component {
+export default class PreviousMedication extends React.Component {
     render () {
         let ddlPreviousTime = [];
         {
