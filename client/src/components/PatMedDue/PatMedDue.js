798439917f8076ed0b2ba686da049e8df2061bf6
unknown <bradwayne74@gmail.com>
2018-04-03 14:23:45 -0400

    upload video card

--- client/src/components/PatMedDue/PatMedDue.js
+++ client/src/components/PatMedDue/PatMedDue.js
@@ -3,6 +3,8 @@ import "./PatMedDue.css";
 
 import { 
     Container,
+    Row,
+    Col,
     Card,
     Button,
     ButtonGroup,
@@ -22,30 +24,29 @@ const PatMedDue = (props) => {
 
     return (
         <Container fluid className="patMedInfo">
-                
-            <Card className="patMedInfoCard" body fluid inverse style={{ backgroundColor: '#2d5366', borderColor: '#2d5366' }}>
-                <CardHeader tag="h4" className="patMedInfoHeader">WHEN YOUR NEXT MEDS ARE DUE</CardHeader>
-                <Card className="patMedInfo">
-                    <CardText className="patMedInfoDue"><h4>Your Next Medication(s) Is Due</h4></CardText>
-                    <br>
-                    </br>
-                    <Label className="text-center patMedDueHours">
-                        In: 2 HOURS
-                    </Label>
-                    <br>
-                    </br>
-                    <Label className="text-center patMedDueTime">
-                        At: 4:00 PM
-                    </Label>
-                    <br>
-                    </br>
+ 
+                <Card className="patMedInfoCard" body fluid inverse style={{ backgroundColor: '#2d5366', borderColor: '#2d5366' }}>
+                    <CardHeader tag="h4" className="patMedInfoHeader">MEDS DUE TIMES</CardHeader>
+                    <Card className="patMedInfoBody">
+                        <CardText className="patMedInfoDue"><h4>Your Next Medication(s) Is Due</h4></CardText>
+                        <br>
+                        </br>
+                        <Label className="text-center patMedDueHours">
+                            In: 2 HOURS
+                        </Label>
+                        <br>
+                        </br>
+                        <Label className="text-center patMedDueTime">
+                            At: 4:00 PM
+                        </Label>
+                        <br>
+                        </br>
 
-                    <div className="patMedDueRemind">
-                        <Button className="patMedRemindBtn" color="secondary" size="lg"><h4>Remind Me!</h4></Button>
-                    </div>
-
+                        <div className="patMedDueRemind">
+                            <Button className="patMedRemindBtn" color="secondary" size="lg"><h4>Remind Me!</h4></Button>
+                        </div>
+                    </Card>
                 </Card>
-            </Card>
 
         </Container>
 
