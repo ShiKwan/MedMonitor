fe925b532933cb883219dc6d45dc89d771a33818
mathewhall100 <mathew.hall@dr.com>
2018-04-06 17:51:53 -0400

    addpatientDrCard bugfix

--- client/src/components/Admin/AddPatientsDrCard/addPatientsDrCard.js
+++ client/src/components/Admin/AddPatientsDrCard/addPatientsDrCard.js
@@ -6,9 +6,8 @@ import {
     Form, FormGroup, Label, Input, FormText,
 } from 'reactstrap';
 
-import FormGroup3_9Name from "../FormGroup/formGroup3_9Name";
-import FormGroup3_9Input from "../FormGroup/formGroup3_9Input";
-import FormGroup3_9Contact from "../FormGroup/formGroup3_9Contact";
+import FormGroup3_9Select from "../FormGroup/formGroup3_9Select";
+
 
 import '../../../pages/Admin';
 
