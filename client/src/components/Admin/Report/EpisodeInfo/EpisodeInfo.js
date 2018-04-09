13526390c7cb8f4eddc52524c1eae432719c86de
mathewhall100 <mathew.hall@dr.com>
2018-04-09 09:45:43 -0400

    previous episode chart functioning

--- client/src/components/Admin/Report/EpisodeInfo/EpisodeInfo.js
+++ client/src/components/Admin/Report/EpisodeInfo/EpisodeInfo.js
@@ -13,7 +13,7 @@ export default class EpisodeInfo extends React.Component {
                 <p>
                     Episode Start : {moment(this.props.episodeDate).format("MMMM Do YYYY")} {moment(this.props.episodeDate).format("hh.mm a")}.
                     <br />
-                    {this.props.episodeCount} records total over {this.props.episodeDays} days.
+                    Episode Data: {this.props.episodeCount} records total over {this.props.episodeDays} days.
                 </p>
             </Container>
 
