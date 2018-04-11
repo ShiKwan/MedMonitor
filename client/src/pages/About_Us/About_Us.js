771719e53b5cd3fc1e31ed9c61b722d659edd9bb
Shi-Kwan <sktan@insytecg.com>
2018-04-11 10:35:31 -0400

    about us page

--- client/src/pages/About_Us/About_Us.js
+++ client/src/pages/About_Us/About_Us.js
@@ -11,48 +11,138 @@ import {
     NavLink,
     Label
 } from 'reactstrap';
-
+import SKHeadShot from "./SK.jpg";
+import MHHeadShot from "./MH.png";
+import BSHeadShot from "./BS.jpg";
+import './about_us.css';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <Container className='text-left'>
-                <Row>
-                    <Col md='4'>
+            <div>
+                <Container className='text-left'>
+                    <h2>About MedMonitor</h2>
+                    <Container>
+                    <p>Inspired by neurologists and their movement disorder patients, MedMonitor is a unique application designed to improve patient care through targeted data analytics.
+                    Through an easy to use patient interface, MedMonitor gathers raw data from Parkinson’s disease patients about their symptoms, 
+                    possible drug side effects and associated medical emergencies, relating these to both time of day and daily medications. </p>
+                    <p>Collated data is then made available to the physician or other healthcare provider through easily visualised, yet information laden, reports and patient summaries. 
+                    Furthermore, the application aids patient compliance with their, often highly complex,  
+                    medication regimen through calendarized reminders and prompts attendance at physician appointments. 
+                    Physician alerts are sent when patients report disease related emergencies.  </p>
+                    <p>MedMonitor is an intuitive and customizable application, designed from the ground up to benefit both patient and physician.  
+                    Patients prefer having a simple yet consistent mechanism for reporting their disease burden over time, 
+                    while healthcare decision makers enjoy access to more reliable, interpretable information to guide patient care. </p>
+                    <p>In addition, with 1-2% of the US population diagnosed with Parkinson’s disease, 
+                    the potentially large volume of data collected by MedMonitor would quickly become a valuable resource for Parkinson’s disease research 
+                    as well as related drug development and monitoring by the neuropharmaceuticals industry.</p>
+                    </Container>
+                </Container>
+                <Container className='text-left'>
+                    <Row>
+                        <Col><h2>Dr. Mathew Hall </h2></Col>
+                    </Row>
+                    <Row>
+                        <Col md='4'>
+                        <img src={MHHeadShot} className='' alt='Dr. Mathew Hall' />
+                        </Col>
+                        <Col md='8'>
+                            <p>Mathew is a full stack web developer with a background in medicine, biomedical research and medical education, 
+                            looking to make a real difference through technology innovation in the medical and biomedical fields.</p>
 
-                    </Col>
-                    <Col md='8'>
-                        Mathew Hall
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='4'>
+                            <p>With a demonstrable track record of lifelong learning, effective communication and entrepreneurship 
+                            Mathew’s problem-solving skills are proven over a wide variety of platforms. </p>
+                            <p>In summary, he describes himself as a tech polymath always looking for a great next project. </p>
+                            <Container>
+                            <Row>
+                                <Col className="text-center">
+                                    <a href='https://github.com/mathewhall100' target='_blank'><i className="fab fa-git"></i></a>
+                                </Col>
+                                <Col className="text-center">
+                                    <a href='http://www.linkedin.com/in/mathew-hall-100' target='_blank'><i className="fab fa-linkedin-in"></i></a>
+                                </Col>
+                            </Row>
+                            </Container>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><h2>Joe Malovasic</h2></Col>
+                    </Row>
+                    <Row>
+                        <Col md='4'>
 
-                    </Col>
-                    <Col md='8'>
-                        Joe Malovasic
-                        Joe is a 26 year old aspiring developer from the Cleveland area. 
-                        After the CWRU bootcamp, he is looking forward to continuing his self education in various programming languages, 
-                        and hoping to eventually dabble in the world of software development.
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='4'>
+                        </Col>
+                        <Col md='8'>
+                            <p>Joe is a 26 year old aspiring developer from the Cleveland area. 
+                            After the CWRU bootcamp, he is looking forward to continuing his self education in various programming languages, 
+                            and hoping to eventually dabble in the world of software development.</p>
+                            <Container>
+                            <Row>
+                                <Col className="text-center">
+                                    <a href='#' target='_blank'><i className="fab fa-git"></i></a>
+                                </Col>
+                                <Col className="text-center">
+                                    <a href='#' target='_blank'><i className="fab fa-linkedin-in"></i></a>
+                                </Col>
+                            </Row>
+                            </Container>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><h2>Bradley Schaeman</h2></Col>
+                    </Row>
+                    <Row>
+                        <Col md='4'>
+                            <img src={BSHeadShot} className='' alt='Bradley Schaeman' />
+                        </Col>
+                        <Col md='8'>
+                            <Container>
+                            <p>
+                                SK's favorite front end guy in this bootcamp!
 
-                    </Col>
-                    <Col md='8'>
-                        Bradley Schaeman
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='4'>
-
-                    </Col>
-                    <Col md='8'>
-                        Shi-Kwan Tan (SK)
-                    </Col>
-                </Row>
-            </Container>
+                            </p>
+                            <Row>
+                                <Col className="text-center">
+                                    <a href='#' target='_blank'><i className="fab fa-git"></i></a>
+                                </Col>
+                                <Col className="text-center">
+                                    <a href='#' target='_blank'><i className="fab fa-linkedin-in"></i></a>
+                                </Col>
+                            </Row>
+                            </Container>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col><h2>Shi-Kwan (SK) Tan</h2></Col>
+                    </Row>
+                    <Row>
+                        <Col md='4'>
+                            <img src={SKHeadShot} className='' alt='Shi-Kwan (SK) Tan' />
+                        </Col>
+                        <Col md='8'>
+                            <Container>
+                            <p>
+                                Full stack application developer graduated from University of Kentucky with B.S. in Computer Science and M.S. in Natural Resources Economics. 
+                            </p>
+                            <p>
+                                A team player that equipped with problem solving skill and eager to learn about the latest technology and incorporate them into web applications.
+                            </p>
+                            <p>
+                                His main goal in tech world is to develop application with great UX and bring positive impacts to the society. 
+                            </p>
+                            <Row>
+                                <Col className="text-center">
+                                    <a href='https://github.com/ShiKwan' target='_blank'><i className="fab fa-git"></i></a>
+                                </Col>
+                                <Col className="text-center">
+                                    <a href='https://www.linkedin.com/in/shi-kwan-tan/' target='_blank'><i className="fab fa-linkedin-in"></i></a>
+                                </Col>
+                            </Row>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         )
     }
 }
